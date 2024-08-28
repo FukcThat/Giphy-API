53335160dc319be5d899c9f7e023a205289a4695
FukcThat <63652e636f6465@gmail.com>
2024-08-28 20:36:40 +0200

    FEAT: Added search functionality

--- gif.js
+++ gif.js
@@ -7,7 +7,7 @@ const searchBtn = document.querySelector(".search-btn");
 // Fetch Function
 const fetchCatGif = () => {
   fetch(
-    "https://api.giphy.com/v1/gifs/translate?api_key=Ynj3YARhHhwDASPE0Qbsj0BpW3ydL6Sb&s=default",
+    "https://api.giphy.com/v1/gifs/translate?api_key=Ynj3YARhHhwDASPE0Qbsj0BpW3ydL6Sb&s=cats",
     { mode: "cors" }
   )
     .then(function (response) {
@@ -18,14 +18,32 @@ const fetchCatGif = () => {
     });
 };
 
-const searchGifs = () => {};
-
-searchBtn.addEventListener("click", () => {
-  searchGifs();
-});
-
 //Event Listeners
 newCatBtn.addEventListener("click", (e) => {
   e.preventDefault();
   fetchCatGif();
 });
+
+const searchGifs = (searchTerm) => {
+  const API_URL = `https://api.giphy.com/v1/gifs/translate?api_key=Ynj3YARhHhwDASPE0Qbsj0BpW3ydL6Sb&s=${searchTerm}`;
+
+  fetch(API_URL, { mode: "cors" })
+    .then((response) => {
+      return response.json();
+    })
+    .then((response) => {
+      img.src = response.data.images.original.url;
+    });
+};
+
+searchBtn.addEventListener("click", () => {
+  searchGifs(getSearchInput());
+});
+
+// Helpers
+
+const getSearchInput = () => {
+  const searchText = searchInput.value;
+  searchInput.value = "";
+  return searchText;
+};
